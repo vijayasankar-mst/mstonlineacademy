fcf123cd40f4c7e971850bc3269782b8b79d76ce
Vijaya Sankar N <sankar.animation@gmail.com>
2016-08-03 13:07:49 +0530

    Added form validation

--- public/scripts/directives/sidebar/sidebar.js
+++ public/scripts/directives/sidebar/sidebar.js
@@ -6,13 +6,22 @@
  * @description
  * # mstApp
  */
-angular.module('myApp')
-	.directive('sidebar',function(){
-		return {
-        templateUrl:'scripts/directives/sidebar/sidebar.html',
-        restrict: 'E',
-        replace: true,
-    	}
-	});
 
+ var myApp = angular.module('myApp');
 
+ myApp.directive('sidebar',function(){
+ 	return {
+ 		templateUrl:'scripts/directives/sidebar/sidebar.html',
+ 		restrict: 'E',
+ 		replace: true,
+ 	}
+ });
+
+ //requestform validataion
+ myApp.controller('requestformController', function($scope) {
+ 	$scope.submitForm = function(isValid) {
+ 		if (isValid) {
+ 			alert('Thanks for submission!');
+ 		}
+ 	};
+ });
