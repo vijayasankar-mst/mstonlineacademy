9b40c32fd3461ee27008d27a0d030480b089229b
Vijaya Sankar N <sankar.animation@gmail.com>
2016-08-16 16:35:47 +0530

    message

--- public/scripts/controllers/registerCtrl.js
+++ public/scripts/controllers/registerCtrl.js
@@ -8,81 +8,81 @@
  */
 
  angular.module('myApp').controller("RegisterCtrl", function($ocLazyLoad,$scope,$state,$rootScope,programServices,$stateParams,$timeout) {
-     $scope.degreelist;
-     $scope.degreeporgramlist;
-     $scope.degreeprogramarealist;
-     $scope.registerFormData;
-     $scope.studentdetails; 
+   $scope.degreelist;
+   $scope.degreeporgramlist;
+   $scope.degreeprogramarealist;
+   $scope.registerFormData;
+   $scope.studentdetails; 
 
-     $scope.getDegreeList = function(){
-         programServices.getDegreeList()
-         .then(function (response) {
-            $scope.degreelist = response.data;
-        }, function (error) {
-            $scope.status = 'Unable to load customer data: ' + error.message;
-        });
-     }
+   $scope.getDegreeList = function(){
+       programServices.getDegreeList()
+       .then(function (response) {
+        $scope.degreelist = response.data;
+    }, function (error) {
+        $scope.status = 'Unable to load customer data: ' + error.message;
+    });
+   }
 
-     $scope.getDegreeProgram = function(){
-         programServices.getDegreePrograms($scope.registerFormData.desireddegree)
-         .then(function (response) {
-            $scope.degreeporgramlist = response.data;
-        }, function (error) {
-            $scope.status = 'Unable to load customer data: ' + error.message;
-        });
-     }
+   $scope.getDegreeProgram = function(){
+       programServices.getDegreePrograms($scope.registerFormData.desireddegree)
+       .then(function (response) {
+        $scope.degreeporgramlist = response.data;
+    }, function (error) {
+        $scope.status = 'Unable to load customer data: ' + error.message;
+    });
+   }
 
-     $scope.getDegreeProgramArea = function(){
-        programServices.getDegreeProgramAreas($scope.registerFormData.desiredprogramarea,$scope.registerFormData.desireddegree)
+   $scope.getDegreeProgramArea = function(){
+    programServices.getDegreeProgramAreas($scope.registerFormData.desiredprogramarea,$scope.registerFormData.desireddegree)
+    .then(function (response) {
+        $scope.degreeporgramarealist = response.data;
+    }, function (error) {
+        $scope.status = 'Unable to load customer data: ' + error.message;
+    });
+}
+
+$scope.getUserInfoDetails = function(){
+    if($stateParams.token !== undefined) {
+        programServices.getStudentDetails($stateParams.token)
         .then(function (response) {
-            $scope.degreeporgramarealist = response.data;
+            console.log("test =", response.data);
+            if(response.data.length > 0){
+               $scope.registerFormData = response.data[0];
+           }else{
+               $scope.errMsg = true;
+               $scope.status = 'Invalid token! You\'ll be redirected in 3 seconds.';
+               $timeout(function() {
+                $state.go("index.home");
+            }, 3000);
+           }
+       }, function (error) {
+        $scope.errMsg = true;
+        $scope.status = 'Token already authenticated ' + error.message;
+    });
+    } else {
+        $state.go("index.home");
+    }
+}
+
+$scope.submitForm = function(isValid){
+    if(isValid) {
+        $scope.registerFormData.authToken = $stateParams.token;
+        $scope.registerFormData.role_id = 3;
+        programServices.saveStudentInformation($scope.registerFormData)
+        .then(function (response) {
+            window.location.href = "index/thanks"
         }, function (error) {
-            $scope.status = 'Unable to load customer data: ' + error.message;
-        });
+           $scope.errMsg = true;
+           $scope.status = 'Username Already Exists';
+       });
     }
+}
 
-    $scope.getUserInfoDetails = function(){
-        if($stateParams.token !== undefined) {
-            programServices.getStudentDetails($stateParams.token)
-            .then(function (response) {
-                console.log("test =", response.data);
-                if(response.data.length > 0){
-                 $scope.registerFormData = response.data[0];
-             }else{
-                 $scope.errMsg = true;
-                 $scope.status = 'Invalid token! You\'ll be redirected in 3 seconds.';
-                 $timeout(function() {
-                    $state.go("index.home");
-                 }, 3000);
-             }
-         }, function (error) {
-            $scope.errMsg = true;
-            $scope.status = 'Token already authenticated ' + error.message;
-        });
-        } else {
-            $state.go("index.home");
-        }
+$scope.errorClass = function(control,method) {
+    if($scope.studentRegisterForm[control].$invalid && !$scope.studentRegisterForm[control].$pristine) {
+        return (method=="class"?'has-error':true);
     }
-
-    $scope.submitForm = function(isValid){
-        if(isValid) {
-            $scope.registerFormData.authToken = $stateParams.token;
-            $scope.registerFormData.role_id = 3;
-            programServices.saveStudentInformation($scope.registerFormData)
-            .then(function (response) {
-                window.location.href = "index/thanks"
-            }, function (error) {
-             $scope.errMsg = true;
-             $scope.status = 'Username Already Exists';
-         });
-        }
-    }
-    
-    $scope.errorClass = function(control,method) {
-        if($scope.studentRegisterForm[control].$invalid && !$scope.studentRegisterForm[control].$pristine) {
-            return (method=="class"?'has-error':true);
-        }
-    }
+}
 
 
     //Phone Number Regex
