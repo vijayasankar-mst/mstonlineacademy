410e2ddc6598d9b40a6f4215dc981bc6656d391b
Vijaya Sankar N <sankar.animation@gmail.com>
2016-10-12 17:06:35 +0530

    Removed unwanted dependencies

--- server/config/config.js
+++ server/config/config.js
@@ -2,28 +2,18 @@ var path = require('path');
 var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
-    development: {
-        mongodb: {
-            db: 'mongodb://localhost/multivision',
-            rootPath: rootPath,
-            port: process.env.PORT || 3060
-        },
-        postgres: {
-            db: 'postgres://ocfwqbfcokhmlm:_UdMkbnKaSUDbDUdN-iDj9CNyG@ec2-54-243-210-223.compute-1.amazonaws.com:5432/d9d7rcm1djt678',
-            rootPath: rootPath,
-            port: process.env.PORT || 3060
+      development: {
+        postgres : {
+           db: process.env.DATABASE_URL,
+           rootPath: rootPath,
+           port: process.env.PORT || 3060
         }
-    },
-    production: {
-        mongodb: {
-            rootPath: rootPath,
-            db: 'mongodb://jeames:multivision@ds053178.mongolab.com:53178/multivision',
-            port: process.env.PORT || 80
-        },
-        postgres: {
-            db: 'postgres://ocfwqbfcokhmlm:_UdMkbnKaSUDbDUdN-iDj9CNyG@ec2-54-243-210-223.compute-1.amazonaws.com:5432/d9d7rcm1djt678',
-            rootPath: rootPath,
-            port: process.env.PORT || 3060
+      },
+      production: {
+         postgres : {
+           db: process.env.DATABASE_URL,
+           rootPath: rootPath,
+           port: process.env.PORT || 3060
         }
-    }
+      }
 }
